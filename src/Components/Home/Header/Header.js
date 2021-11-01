e556c792402400905bba0233c2a8164b31edab49
safidark <safi29317@gmail.com>
2021-11-01 10:41:36 +0600

    shafi

--- src/Components/Home/Header/Header.js
+++ src/Components/Home/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Navbar from '../Navbar/Navbar';
+import './Header'
 const Header = () => {
     return (
-        <div>
-            
+        <div className="headerBackground">
+            <Navbar/>
         </div>
     );
 };

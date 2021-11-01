60653d26ce6da300e8bc03198eb1384dcc006d1b
Raktim02 <you@example.com>
2021-11-01 12:02:07 +0600

    raktim

---
+++ src/Components/Home/Example/Example.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Example.scss';
+
+const Example = () => {
+    return (
+        <div className="py-4">
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Example;
--- src/Components/Home/Index/Main.js
+++ src/Components/Home/Index/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
+import About from '../About/About';
+import Example from '../Example/Example';
 
 const Main = () => {
     return (
         <div>
             <Header/>
+            <About/>
+            <Example/>
         </div>
     );
 };

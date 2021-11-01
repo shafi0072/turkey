9501432206413272d8cb0231cf06a2f38334971c
Raktim02 <you@example.com>
2021-11-01 11:26:21 +0600

    navbar

--- src/Components/Home/Navbar/Navbar.js
+++ src/Components/Home/Navbar/Navbar.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Navbar.scss'
 const Navbar = () => {
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light navbar-container">
-                <a className="navbar-brand" href="#">Navbar</a>
+        <div className="navbar-container">
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <a className="navbar-brand" href="#">THE SHARK</a>
                 <button
                     className="navbar-toggler"
                     type="button"
@@ -15,21 +15,21 @@ const Navbar = () => {
                     aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className="collapse navbar-collapse navbar-menu-container" id="navbarNav">
                     <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Home
+                        <li className="nav-item nav-active">
+                            <a className="nav-link nav-active" href="#">The Shark
                                 <span className="sr-only">(current)</span>
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
+                            <a className="nav-link" href="#">Easi-Evaluation in Ayres Sensory integration</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
+                            <a className="nav-link" href="#">Contact</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link disabled" href="#">Disabled</a>
+                            <a className="nav-link" href="#">English</a>
                         </li>
                     </ul>
                 </div>

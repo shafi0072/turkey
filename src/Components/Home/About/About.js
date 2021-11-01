4ad8d7dd7ff8eea3ab881e1bc50c80d541e09776
safidark <safi29317@gmail.com>
2021-11-01 12:54:10 +0600

    shafi

--- src/Components/Home/About/About.js
+++ src/Components/Home/About/About.js
@@ -1,30 +1,60 @@
 import React from 'react';
+import { Typography, Box, Button, Grid, Container } from "@mui/material";
+
 import Img1 from '../../../Resorce/images/Group 8.png';
 import './About.scss';
 
 const Who = () => {
     return (
-        <div className="py-4 about-container">
-            <div className="row">
-                <div className="col-md-7">
-                    <div>
-                        <h2><span className="bottom-border">QUI NOUS</span> SOMMES</h2>
-                        <p>Mekanisk se distingue par son service professionnel et personnalisé en conception et impression 3D, vous permettant d'obtenir les pièces industrielles et fonctionnelles requises pour vos projets.
-                        <br/>
-                        <br/>
-                        Passionnés par les projets de développement de produits en tout genre, nous pouvons vous supporter de la phase conceptuelle jusqu’à la production de votre produit.
-                        <br/>
-                        <br/>
-                        Que vous souhaitiez valider votre concept préliminaire auprès de clients potentiels, réduire vos coûts ou votre « time to market », valider l'ergonomie de votre conception, ou confirmer la faisabilité de votre projet, nous pouvons vous aider à profiter des solutions innovantes disponibles. Si nous n'avons pas la technologie dont vous avez besoin, vous pouvez profitez de nos contacts avec nos fournisseurs de confiance, tout en respectant vos coûts et échéanciers!</p>
-                    </div>
-                </div>
-                <div className="col-md-5">
-                    <div>
-                        <img src={Img1} alt=""/>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div className="quisection">
+        <Box my={30} className="quimargn">
+          <Container>
+            <Grid container>
+              <Grid item lg={6} md={12} sm={12} xs={12}>
+                <Typography variant="h3" gutterBottom>
+                  QUI NOUS SOMMES
+                </Typography>
+                <div className="greenbottom"></div>
+                <br />
+                <br />
+                <Typography variant="p" gutterBottom color="darkgray">
+                  Mekanisk se distingue par son service professionnel et
+                  personnalisé en conception et impression 3D, vous permettant
+                  d'obtenir les pièces industrielles et fonctionnelles requises
+                  pour vos projets.
+                </Typography><br />
+                <Typography variant="p" gutterBottom color="darkgray">
+                  Passionnés par les projets de développement de produits en tout
+                  genre, nous pouvons vous supporter de la phase conceptuelle
+                  jusqu’à la production de votre produit.
+                </Typography><br />
+                <Typography variant="p" gutterBottom color="darkgray">
+                  Que vous souhaitiez{" "}
+                  <span className="font-f-sm"> valider votre concept </span>{" "}
+                  préliminaire auprès de clients potentiels,{" "}
+                  <span className="font-f-sm"><br />
+                    réduire vos coûts ou votre « time to market », valider
+                    l'ergonomie{" "}
+                  </span>{" "} <br />
+                  de votre conception, ou{" "}
+                  <span className="font-f-sm">confirmer la faisabilité </span> <br /> de
+                  votre projet, nous pouvons vous aider à profiter des solutions
+                  innovantes disponibles. Si nous n'avons pas la technologie dont
+                  vous avez besoin, vous pouvez{" "}
+                  <span className="font-f-sm"> profitez de nos </span> <br /> contacts
+                  avec nos fournisseurs de confiance, tout en respectant vos coûts
+                  et échéanciers <br />!
+                </Typography>
+              </Grid>
+              <Grid item lg={6} md={12} sm={12} xs={12}>
+                <Box textAlign="center">
+                  <img src={Img1} width="80%" height="auto" />
+                </Box>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+      </div>
     );
 };
 
